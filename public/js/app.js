1a66233cc6bb646b565669f4b0308b6d000c7135
JD <jdfili14@gmail.com>
2019-03-22 23:46:06 -0700

    stlyed a lil bit

--- public/js/app.js
+++ public/js/app.js
@@ -11,16 +11,6 @@ $(document).ready(function () {
         } else {
             $(this).addClass("clicked");
         }
-        $.ajax({
-            method: "GET",
-            url: "/articles/" + form
-        }).then(function (res) {
-            // console.log(res.comment.title);
-            // console.log(res.comment.body);
-            // $("#comment-title" + form).append(res.comment.title);
-            // $("#comment-body" + form).append(res.comment.body);
-            console.log(res);
-        })
     });
     $(".submit").on("click", function () {
         var value = $(this).attr("data-id");
@@ -28,22 +18,27 @@ $(document).ready(function () {
             title: $("#title-input" + value).val().trim(),
             body: $("#body-input" + value).val().trim()
         };
-        console.log(newComment)
-        var id = $(this).attr("data-id");
-        $.ajax({
-            method: "POST",
-            url: "/articles/" + id,
-            data: newComment
-        }).then(function () {
-            console.log("posted");
-        });
+        if (newComment.title === "" || newComment.body === "") {
+            alert("Please fill out all text fields.")
+        }
+        else {
+            var id = $(this).attr("data-id");
+            $.ajax({
+                method: "POST",
+                url: "/articles/" + id,
+                data: newComment
+            }).then(function () {
+                location.reload();
+            });
+        }
     });
-    $(".delete").on("click", function(){
+
+    $(".delete").on("click", function () {
         var id = $(this).attr("data-id");
         $.ajax({
             method: "DELETE",
             url: "/articles/" + id
-        }).then(function(){
+        }).then(function () {
             location.reload();
         })
     })
